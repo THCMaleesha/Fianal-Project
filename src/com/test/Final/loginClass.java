a9ab18399094ba4d374f767df111c61d2bfb2766
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2021-07-10 22:16:22 +0530

    set data validations for logins

--- src/com/test/Final/loginClass.java
+++ src/com/test/Final/loginClass.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 
 public class loginClass {
 
-    static String id,firstName,lastName,emailgot,pword,cont,address;
+    static String id,firstName,lastName,emailgot;
 
     public void userLogin(String email, char[] password,JFrame msgFrame) {
 
@@ -24,23 +24,47 @@ public class loginClass {
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            id = resultSet.getString(1);
-            firstName = resultSet.getString(2);
-            lastName = resultSet.getString(3);
-            emailgot = resultSet.getString(5);
-
             int count = 0;
             while (resultSet.next()){
                 count += 1;
             }
             if (count == 1){
                 JOptionPane.showMessageDialog(msgFrame,"Login Successful !!!");
+                userDetails(email,password,msgFrame);
                 loginFrame.dispose();
                 new userpageClass(id,firstName,lastName,emailgot);
 
             }else{
                 JOptionPane.showMessageDialog(msgFrame,"ERROR !!!");
             }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(msgFrame,"Oops !!!\nSomething went Wrong !!!\n"+exception.getMessage());
+        }
+    }
+
+    public void userDetails(String email, char[] password,JFrame msgFrame) {
+
+        try {
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "SELECT * FROM `customers_table` WHERE email_Address = ? and Password = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2, String.valueOf(password));
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                id = resultSet.getString(1);
+                firstName = resultSet.getString(2);
+                lastName = resultSet.getString(3);
+                emailgot = resultSet.getString(5);
+            }
+
             resultSet.close();
             preparedStatement.close();
 
--- src/com/test/Final/userpageClass.java
+++ src/com/test/Final/userpageClass.java
@@ -5,20 +5,50 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class userpageClass {
+
+    static String email, password;
+
+    public void orderDetails(String cusID, JFrame frame){
+        try {
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "SELECT * FROM `orders_table` WHERE cus_ID = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1,cusID);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            DefaultTableModel table = (DefaultTableModel)table1.getModel();
+            table.setRowCount(0);
+
+            while (resultSet.next()){
+                Object object[] = {resultSet.getString(1),resultSet.getString(2),resultSet.getString(3)};
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(frame,"Oops !!!\nSomething went Wrong !!!\n"+exception.getMessage());
+        }
+
+        }
+
     private JPanel userPanel;
     private JLabel cusIDlabel;
     private JLabel cusNameLabel;
     private JLabel emailLabel;
-    private JTable table1;
     private JButton settingsButton;
     private JButton buyNowButton;
     private JButton CANCELButton;
     private JLabel totamountLabel;
     private JLabel amountLabel;
     private JButton orderHisButton;
-    private JScrollPane tablePanel;
+    private JTable table1;
     private JFrame userframe;
 
     public userpageClass(String id,String firstName,String lastName,String emailgot){
@@ -38,12 +68,22 @@ public class userpageClass {
         cusNameLabel.setText(firstName+lastName);
         emailLabel.setText(emailgot);
 
+
+
         CANCELButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 userframe.dispose();
                 new loginClass();
+            }
+        });
+        orderHisButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new JFrame();
+                orderDetails(id,frame);
+
             }
         });
     }
